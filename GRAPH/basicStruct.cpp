da41252a4035cf37b8af2e12fade673bcc123353
Raghav-28-Gupta <raghavgupta07159@gmail.com>
2025-03-25 18:40:58 +0530

    Added Graph

--- GRAPH/basicStruct.cpp
+++ GRAPH/basicStruct.cpp
@@ -17,7 +17,7 @@
 using namespace std;
 
 
-class Graph{
+class GraphII{
      public:
           unordered_map<int, list<int>> adjList;
 
@@ -57,10 +57,58 @@ class Graph{
 
           }
 
+
+          // Cycle Detection
+          bool cycleDetectionUndirectedBFS(int src, unordered_map<int, bool> &visited){
+               queue<int> q;
+               unordered_map<int, int> parent;
+
+               // Initial state
+               q.push(src);
+               visited[src] = true;
+               parent[src] = -1;
+
+               while(!q.empty()) {
+                    int frontNode = q.front();
+                    q.pop();
+
+                    for(auto nbr : adjList[frontNode]) {
+                         if(!visited[nbr]) {
+                              q.push(nbr);
+                              visited[nbr] = true;
+                              parent[nbr] = frontNode;
+                         }
+
+                         // Cycle Detection case
+                         else if(visited[nbr] == true && nbr != parent[frontNode]) {
+                              return true;
+                         }
+                    }
+               }
+
+               return false;
+          }
+          bool isCycle(int n) {
+               int src = 0;
+               unordered_map<int, bool> visited;
+               
+               // // if Graph contains components
+               // bool ans = false;
+               // for(int src = 0; src < n; src++) {
+               //      if(!visited[src]) {
+               //           ans = cycleDetectionUndirectedBFS(src, visited);
+               //      }
+               //      if(ans == true) break
+               // }
+               // return ans;
+
+               return cycleDetectionUndirectedBFS(src, visited);
+          }
+
 };
 
-
-class GraphII{
+// Weighted Graph
+class Graph{
      public:
           unordered_map<int, list<pair<int, int>>> adjList;
 
@@ -76,7 +124,6 @@ class GraphII{
                     adjList[u].push_back({v, weight});
                }
           }
-          
           void printAdjList(int n){
                for(int i = 0; i < n; i++) {
                     cout << i << ": ";
@@ -99,26 +146,101 @@ class GraphII{
                // }
 
           }
+          
+          // BFS Traversal
+          void BFSTraversal(int src) {
+               queue<int> q;
+               unordered_map<int, bool> visited;
+               
+               // Initial state
+               q.push(src);
+               visited[src] = true;
+
+               while(!q.empty()) {
+                    int frontNode = q.front();
+                    cout << frontNode << ", ";
+                    q.pop();
+
+                    for(auto nbr : adjList[frontNode]) {
+                         // nbr -> pair
+                         // first -> node
+                         int node = nbr.first;
+                         // second -> weight
+                         int weight = nbr.second;
+                         if(!visited[node]) {
+                              q.push(node);
+                              visited[node] = true;
+                         }
+                    }
+               }
+
+               cout << endl;
+          }
+          
+          // DFS Traversal
+          void DFSTraversal(int src, unordered_map<int,bool> &visited) {
+               visited[src] = true;
+               cout << src << ", ";
+
+               for(auto nbr : adjList[src]) {
+                    // nbr -> pair
+                    // first -> node
+                    int node = nbr.first;
+                    // second -> weight
+                    int weight = nbr.second;
+                    if(!visited[node]){
+                         DFSTraversal(node, visited);
+                    }
+               }
+
+
+          } 
+          void dfs(int n){
+               int src = 0;
+               unordered_map<int,bool> visited;
+               
+               // for Component Graph
+               // for(int src = 0; src < n; src++) {
+               //      if(!visited[src]) {
+               //           DFSTraversal(src, visited);
+               //      }
+               // }
+
+               DFSTraversal(src, visited);
+          } 
+
+          // Cycle Detection Undirected in GraphII
+          
+
+
 
 };
 
 
 int main() {
-     // Graph g;
-     // g.addEdge(0, 1, 1);
-     // g.addEdge(0, 2, 1);
-     // g.addEdge(1, 2, 1);
-     // g.addEdge(2, 3, 1);
-     // int n = 4;
-     // g.printAdjList(4);
-
      GraphII g;
-     g.addEdge(0, 1, 3, 1);
-     g.addEdge(0, 2, 6, 1);
-     g.addEdge(1, 2, 3, 1);
-     g.addEdge(2, 3, 2, 1);
+     g.addEdge(0, 1, 0);
+     g.addEdge(1, 2, 0);
+     g.addEdge(3, 0, 0);
+     g.addEdge(2, 3, 0);
      int n = 4;
      g.printAdjList(4);
+     cout << "does it contain cycle: " << boolalpha << g.isCycle(4);
+
+     // Graph g;
+     // g.addEdge(0, 3, 3, 1);
+     // g.addEdge(0, 5, 3, 1);
+     // g.addEdge(0, 2, 3, 1);
+     // g.addEdge(2, 5, 3, 1);
+     // g.addEdge(3, 5, 3, 1);
+     // g.addEdge(5, 4, 3, 1);
+     // g.addEdge(5, 6, 3, 1);
+     // g.addEdge(4, 1, 3, 1);
+     // g.addEdge(6, 1, 3, 1);
+     // int n = 7;
+     // g.printAdjList(n);
+     // cout << endl << "BFS: "; 
+     // g.dfs(0);
 
      return 0;
 

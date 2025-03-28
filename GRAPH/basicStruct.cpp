42330840f0f2c2e3b50910703cfe96000fa34694
Raghav-28-Gupta <raghavgupta07159@gmail.com>
2025-03-28 18:52:33 +0530

    Added topologicalSort using BFS

--- GRAPH/basicStruct.cpp
+++ GRAPH/basicStruct.cpp
@@ -165,10 +165,40 @@ class GraphII{
                ans.push_back(src);
           }
          
+          void topBfs(int n, vector<vector<int>>& adj, vector<int> &ans) {
+               unordered_map<int, int> indegree;
+               queue<int> q;
+               
+               // Preparing n-degree map
+               for(int src = 0; src < n; src++){
+                    for(auto nbr : adj[src]){
+                         indegree[nbr]++;
+                    }
+               }
+               
+               // Maintaing initia; state in q
+               for(int i = 0; i < n; i++) {
+                    if(indegree[i] == 0) {
+                         q.push(i);
+                    }
+               }
+               
+               while(!q.empty()) {
+                    int frontNode = q.front();
+                    ans.push_back(frontNode);
+                    q.pop();
+                    
+                    // Since FrontNode is removed 
+                    // Update the indegree of its children accordingly
+                    for(int nbr : adj[frontNode]) {
+                         indegree[nbr]--;
+                         if(indegree[nbr] == 0){
+                              q.push(nbr);
+                         }
+                    }
+               }
+          }
 
-         
-
-          // Function to return list containing vertices in Topological order.
           vector<int> topologicalSort(vector<vector<int>>& adj) {
                // Your code here
                unordered_map<int, bool> vis;
@@ -176,9 +206,9 @@ class GraphII{
                int n = adj.size();
                
                for(int i = 0; i < n; i++) {
-                   if(!vis[i]) {
-                       topDfs(adj, i, vis, ans);
-                   }
+                    if(!vis[i]) {
+                         topDfs(adj, i, vis, ans);
+                    }
                }
                
                reverse(ans.begin(), ans.end());
@@ -186,6 +216,7 @@ class GraphII{
           }
 
 
+          
 
 };
 

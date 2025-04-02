e7f34b7f1ff8c352c67ed82e9b4004b5d1b85ef1
Raghav-28-Gupta <raghavgupta07159@gmail.com>
2025-04-02 07:15:07 +0530

    Bridge edge in a graph

---
+++ GRAPH/bridgeEdge.cpp
@@ -0,0 +1,87 @@
+#include <iostream>
+#include <vector>
+#include <algorithm> 
+#include <climits>
+#include <set>
+#include <unordered_set>
+#include <list>
+#include <map>
+#include <stack>
+#include <queue>
+#include <math.h>
+#include <limits.h>
+#include <cmath>
+#include <unordered_map>
+#include <utility>
+#include <numeric>
+using namespace std;
+
+
+// GFG : Bridge edge in a graph
+void isBridgeDfs(int src, int parent, vector<int> adj[], vector<int> &tin, vector<int> &low, unordered_map<int, bool> &vis, int &isBridge, int &timer, int c, int d){
+     timer++;
+     vis[src] = true;
+     tin[src] = timer;
+     low[src] = timer;
+     
+     for(auto nbr: adj[src]) {
+          if(nbr == parent) continue;  // Ignore parent edge
+          
+          if(!vis[nbr]) {
+               isBridgeDfs(nbr, src, adj, tin, low, vis, isBridge, timer, c, d);
+               
+               // Update low value after returning from recursion
+               low[src] = min(low[src], low[nbr]);
+               
+               // Check if (node, neighbor) is a bridge
+               if(low[nbr] > tin[src]) {
+                    if((nbr == c && src == d) || (nbr == d && src == c)) {
+                         isBridge = 1;
+                    }
+               }
+          }
+          
+          else{
+               // Back edge case
+               low[src] = min(low[src], tin[nbr]);
+          }
+     }
+     
+ }
+ 
+int isBridge(int V, vector<int> adj[], int c, int d) {
+     // Code here
+     vector<int> tin(V);
+     vector<int> low(V);
+     int timer = 0;
+     unordered_map<int, bool> vis;
+     int isBridge = 0;
+     
+     for(int i = 0; i < V; i++){
+          if(!vis[i]) {
+               isBridgeDfs(i, -1, adj, tin, low, vis, isBridge, timer, c, d);
+          }
+     }
+     
+     return isBridge;
+}
+
+
+// LEETCODE : 1192 (Critical Connections in a Network)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

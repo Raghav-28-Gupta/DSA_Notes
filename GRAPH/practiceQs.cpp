4677d23841556f9521de84ff183c6b34d912d043
Raghav-28-Gupta <raghavgupta07159@gmail.com>
2025-04-26 15:28:04 +0530

    Add alien dictionary implementation using topological sorting

--- GRAPH/practiceQs.cpp
+++ GRAPH/practiceQs.cpp
@@ -783,6 +783,81 @@ vector<int> findOrder(int N, vector<vector<int>> prerequisites) {
 
 
 
+// GFG : Alien Dictionary   [*Topological Sort Application*]
+vector<int> topoOrder(int N, unordered_map<int, vector<int>> &adjList, unordered_map<int, int> inDegree) {
+    queue<int> q;
+    for(auto &entry: inDegree) {
+        if(entry.second == 0) {
+        q.push(entry.first);
+        }
+    }
+
+    vector<int> topoSort;
+    while(!q.empty()){
+        int node = q.front();
+        q.pop();
+        topoSort.push_back(node);
+
+        for(auto nbr : adjList[node]) {
+            // conceptually "remove" the outgoing edges
+            // of node with inDegree '0'
+            inDegree[nbr]--;
+            if(inDegree[nbr] == 0) {
+            q.push(nbr);
+            }
+        }
+    }
+
+    return topoSort;
+}
+
+string findOrder(vector<string> &words) {
+    // used map to optimize the length of adjList && inDegree
+    unordered_map<int, vector<int>> adjList;
+    unordered_map<int, int> inDegree;
+    unordered_set<int> vis;
+
+    // to check topo size
+    for(auto word : words) {
+        for(auto it : word) {
+            vis.insert(it - 'a');
+            if(!inDegree.count(it - 'a')){
+            inDegree[it - 'a'] = 0;
+            }
+        }
+    }
+
+    for(int i = 0; i < words.size() - 1; i++){
+        string st1 = words[i];
+        string st2 = words[i + 1];
+        int len = min(st1.length(), st2.length());
+
+        if (st1.substr(0, len) == st2.substr(0, len) && st1.length() > st2.length()) {
+        return ""; // Invalid prefix case
+        }
+
+        for(int j = 0; j < len; j++) {
+            if(st1[j] != st2[j]) {
+            adjList[st1[j] - 'a'].push_back(st2[j] - 'a');
+            inDegree[st2[j] - 'a']++;
+            break;
+            }
+        }
+    }
+
+
+    vector<int> topoSort = topoOrder(26, adjList, inDegree);
+
+    if(topoSort.size() != vis.size()) return "";
+
+    string ans = "";
+    for(auto it : topoSort) {
+        ans = ans + char(it + 'a');
+    }
+
+    return ans;
+}
+
 
 
 
